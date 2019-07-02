ae8a7244e42d99eaccd0f9a66dd2a1231482bd55
Rebecca <rebsomach@gmail.com>
2019-07-02 16:05:40 -0400

    Added control of angle range

--- ellipse.m
+++ ellipse.m
@@ -5,18 +5,21 @@ classdef ellipse < handle
         yc
         a
         b
+        %Range of angles that we can set
+        qVector
     end
     
     methods
-        function this = ellipse(x,y,xscale,yscale)
+        function this = ellipse(x,y,xscale,yscale,q)
             this.xc = x;
             this.yc = y;
             this.a = xscale;
             this.b = yscale;
+            this.qVector = q;
         end
         
         function render(this)
-            t = [0:pi/500:2*pi];
+            t = this.qVector;
             x = this.xc + this.a*cos(t);
             y = this.yc + this.b*sin(t);
             plot(x,y,'LineWidth',2);
